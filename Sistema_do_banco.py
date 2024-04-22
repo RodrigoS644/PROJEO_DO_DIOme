21e9b2c425ed1f45bee8611e0ad2011655841b89
Rodrigo de sousa barbosa <167349825+RodrigoS644@users.noreply.github.com>
2024-04-21 21:20:31 -0300

    ND

---
+++ Sistema_do_banco.py
@@ -0,0 +1,64 @@
+
+menu = """""
+################ MENU ################
+
+{"d"} Depositar
+{"s"} Sacar
+{"e"} Extrato
+{"q"} Sair
+
+===>
+################ FIM ##################
+"""
+menu_deposito= """"
+
+################ DEPOSITO ################
+                               
+        DIGITE O VALOR DE DEPOSITO:         
+                            
+################# FIM ####################
+
+
+"""
+menu_saque = """
+
+################ SAQUE ################
+                               
+        DIGITE O VALOR DE SAQUE:         
+                            
+################# FIM #################
+
+"""
+
+menu_extrato = """"
+
+
+
+"""
+
+saldo = 0
+limite =500
+extrato=""
+numero_saques = 3
+
+
+while True:
+   opcao = input("AQUI")
+
+   if opcao=="d":
+      print(menu_deposito)   
+     
+
+   elif opcao=="s":
+      print(menu_saque)
+
+   elif opcao=="e":
+      print("Extrato")
+
+
+   elif opcao=="q":
+      break
+
+
+   else :
+      print("operacao invalida , por favor selecione novante a operacao desejada")
