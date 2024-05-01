140a890e6d896e30a0631b03d9aeb5f35c5be1ef
Rodrigo de sousa barbosa <167349825+RodrigoS644@users.noreply.github.com>
2024-05-01 10:05:38 -0300

    dd

---
+++ orientacao _a_objeto.py
@@ -0,0 +1,10 @@
+
+while True :
+ NUMEROS = ("um", "dois", "tres", "quatro","cinco", "seis","sete" ,"oito", "nove" ,"dez", "onde" ,"doze", "treze", "catoze", "quinze", "deseseis","desesete", "desoito", "desenove", "vinte" )
+
+ b =input("digite um nuemro de 0 a 20; ")
+
+ print(NUMEROS[(int(b)-1)])
+
+ # list.append("none") no final
+ # list.insert(0,"none") no numero sem apagar
